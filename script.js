5cae667ae6db57b4716676e0fae848c6936d88f0
Luskahenriqq <lucasrico2005@gmail.com>
2021-12-01 15:20:14 -0300

    termina o jogo e anuncia vencedor

--- script.js
+++ script.js
@@ -4,28 +4,42 @@ var vencedor = "";
 $(document).ready(function(){
 $(".casa").click(function(){
   var fundo = $(this).css("background-image");
-  if(fundo == "none"||fundo == ""){
-  var fig = "url("+vez.toString()+".jpg)";
+  if(fundo == "none" || fundo == ""){
+  var fig = "url( " + vez.toString() + ".jpg)";
   $(this).css("background",fig);
   vez = (vez == 1 ? 2:1);
   verificarFimDeJogo();
+}
 
+  function verificarFimDeJogo(){
+  var existemCasasIguais = [verificaSeHaCasasIguais(1,2,3), verificaSeHaCasasIguais(4,5,6),
+     verificaSeHaCasasIguais(7,8,9), verificaSeHaCasasIguais(1,4,7), verificaSeHaCasasIguais(2,5,8),
+     verificaSeHaCasasIguais(3,6,9), verificaSeHaCasasIguais(1,5,9), verificaSeHaCasasIguais(3,5,7)];
+     if(existemCasasIguais.indexOf(true) >= 0){
+     $("#resultado").html("<h1>O jogador " + vencedor + " venceu!</h1>");
+     $(".casa").off("click");
+     }
   }
+
+  function verificaSeHaCasasIguais(numeroPrimeiraCasa, numeroSegundaCasa, numeroTerceiraCasa){
+    var primeiraCasa = $("#casa" + numeroPrimeiraCasa);
+    var segundaCasa = $("#casa" + numeroSegundaCasa);
+    var terceiraCasa = $("#casa" + numeroTerceiraCasa);
+    var fundoPrimeiraCasa = $("#casa" + numeroPrimeiraCasa).css("background-image");
+    var fundoSegundaCasa = $("#casa" + numeroSegundaCasa).css("background-image");
+    var fundoTerceiraCasa = $("#casa" + numeroTerceiraCasa).css("background-image");
+    if((fundoPrimeiraCasa == fundoSegundaCasa) && (fundoSegundaCasa == fundoTerceiraCasa) && (fundoPrimeiraCasa != "none" && fundoPrimeiraCasa != "")){
+    if(fundoPrimeiraCasa.indexOf ("1.jpg") >= 0) {
+      vencedor = "1";
+      return true;
+    }else{
+      vencedor = "2";
+      return true;
+    }
+    }else{
+      return false;
+    }
+  }
+
 });
 });
-function verificarFimDeJogo(){
-
-}
-function verificaSeHaCasasIguais(numeroPrimeiraCasa, numeroSegundaCasa, numeroTerceiraCasa){
-  var primeiraCasa = $("#casa" + numeroPrimeiraCasa);
-  var segundaCasa = $("#casa" + numeroSegundaCasa);
-  var terceiraCasa = $("#casa" + numeroTerceiraCasa);
-  var fundoPrimeiraCasa = $("#casa" + numeroPrimeiraCasa).css("background-image");
-  var fundoSegundaCasa = $("#casa" + numeroSegundaCasa).css("background-image");
-  var fundoTerceiraCasa = $("#casa" + numeroTerceiraCasa).css("background-image");
-  if((fundoPrimeiraCasa == fundoSegundaCasa) && (fundoSegundaCasa == fundoTerceiraCasa) && (fundoPrimeiraCasa !& = "none" && fundoPrimeiraCasa != "")){
-
-  } else {
-    return false;
-  }
-}
